616505636999b7549cde0db89a4bd115eeaa3474
Rohit <rohitgupta9886@gmail.com>
2023-09-06 22:49:53 +0530

    commiting from STS

---
+++ src/main/java/TestApp.java
@@ -0,0 +1,10 @@
+//import java.util.logging.Logger;
+import org.apache.logging.log4j.Logger;
+public class TestApp {
+	private static Logger log=Logger.getLogger(TestApp.class.getName());
+	////Appender app=new ConsoleAppender(laypot);
+	public static void main(String[] args) {
+		
+	}
+
+}
